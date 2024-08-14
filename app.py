544951d16c7aee49d4702c73803360fbb3399eb6
ahaseeb235 <ahaseeb235@gmail.com>
2024-08-14 01:08:11 +0100

    testing files

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    return"Hello world"
+from application import app
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug = True) 
+
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def index():
+#     return"Hello world"
+
+# if __name__ == "__main__":
+#     app.run(debug = True)
--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,5 @@
 from application import app
 
-
-
 @app.route("/")
 def index():
     return"""
