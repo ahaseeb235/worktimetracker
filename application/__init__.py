64c677baba7baf60012a45afb9c2e816e6a3651a
ahaseeb235 <ahaseeb235@gmail.com>
2024-08-17 20:38:52 +0100

    adding db file

--- application/__init__.py
+++ application/__init__.py
@@ -3,9 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///worktimeDB.db'
 app.config['SECRET_KEY'] = 'pa55word'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///worktimeDB.db'
+
 
 db = SQLAlchemy(app)
+app.app_context().push() # adding context
 
 from application import routes
